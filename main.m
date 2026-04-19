f875fd00b8c191d434bd5338e1198149778c1bdb
Andynium <ender3info@gmail.com>
2026-04-19 16:37:18 +0200

    Xcode project created

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Leodo
+//
+//  Created by Administrator on 19.04.26.
+//  Copyright __MyCompanyName__ 2026. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
